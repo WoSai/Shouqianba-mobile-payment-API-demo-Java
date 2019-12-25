b85dec3c64a0088465c99abba1587f297e5441d5
lyx <1243160287@qq.com>
2019-12-25 16:38:25 +0800

    upload

--- src/main/java/com/wosai/sqb/storemap/HttpProxy.java
+++ src/main/java/com/wosai/sqb/storemap/HttpProxy.java
@@ -52,7 +52,7 @@ public class HttpProxy {
         try{
             params.put("appid",appid);                                   //appid，必填
             params.put("code",code);                                     //激活码，必填
-            params.put("device_id","CNHM0001POS01");                     //客户方收银终端序列号，需保证同一appid下唯一，必填。为方便识别，建议格式为“品牌名+门店编号+‘POS’+POS编号“
+            params.put("device_id","");                     //客户方收银终端序列号，需保证同一appid下唯一，必填。为方便识别，建议格式为“品牌名+门店编号+‘POS’+POS编号“
             params.put("client_sn","POS01");                             //客户方终端编号，一般客户方或系统给收银终端的编号，必填
             params.put("name","1号款台");                                 //客户方终端名称，必填
             params.put("os_info","Mac OS");                              
@@ -85,7 +85,7 @@ public class HttpProxy {
         JSONObject params = new JSONObject();
         try{
             params.put("terminal_sn",terminal_sn);                       
-            params.put("device_id","CNHM0001POS01");                     
+            params.put("device_id","");                     
             params.put("os_info","Mac OS");                              
             params.put("sdk_version","Java SDK v1.0");	                 
             String sign = getSign(params.toString() + terminal_key);
@@ -117,7 +117,7 @@ public class HttpProxy {
             params.put("client_sn",getClient_Sn(16));  //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
             params.put("total_amount","1000");               //交易总金额,以分为单位
             params.put("payway","1");	                     //支付方式,1:支付宝 3:微信 4:百付宝 5:京东钱包
-            params.put("dynamic_id","130818341921441147");	 //条码内容
+            params.put("dynamic_id","");	 //条码内容
             params.put("subject","Pizza");	                 //交易简介
             params.put("operator","kay");	                 //门店操作员
 
@@ -140,7 +140,7 @@ public class HttpProxy {
         try{
             params.put("terminal_sn",terminal_sn);            //收钱吧终端ID
             params.put("sn","7892259488292938");              //收钱吧系统内部唯一订单号
-            params.put("client_sn","18348290098298292838");   //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
+            params.put("client_sn","123456789");   //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
             params.put("refund_amount","1000");               //退款金额
             params.put("refund_request_no","23030349");	      //商户退款所需序列号,表明是第几次退款
             params.put("operator","kay");	                  //门店操作员
@@ -166,7 +166,7 @@ public class HttpProxy {
         try{
             params.put("terminal_sn",terminal_sn);           //终端号
             params.put("sn","7892259488292938");             //收钱吧系统内部唯一订单号
-            params.put("client_sn","18348290098298292838");  //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
+            params.put("client_sn","123456789");  //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
 
             String sign = getSign(params.toString() + terminal_key);
             String result = HttpUtil.httpPost(url, params.toString(),sign,terminal_sn);
@@ -188,7 +188,7 @@ public class HttpProxy {
         try{
             params.put("terminal_sn",terminal_sn);           //终端号
             params.put("sn","7892259488292938");             //收钱吧系统内部唯一订单号
-            params.put("client_sn","18348290098298292838");  //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
+            params.put("client_sn","123456789");  //商户系统订单号,必须在商户系统内唯一；且长度不超过64字节
 
             String sign = getSign(params.toString() + terminal_key);
             String result = HttpUtil.httpPost(url, params.toString(),sign,terminal_sn);
